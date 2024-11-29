b38779deefd251a1b7bd812fe39d902cb1ed00ad
vishal kushwah <kushwahvishal2391@gmail.comm>
2024-11-29 22:49:29 +0530

    first node

---
+++ 07 nov  node/index.js
@@ -0,0 +1,18 @@
+const express= require("express");
+const app=express();
+const Port=9000;
+app.get("/",(req,res)=>{
+    res.send("<h1> My First page  </h1>");
+})
+
+app.get("/home",(req,res)=>{
+    res.send("My Home page this is first page");
+})
+
+app.get("/about",(req,res)=>{
+    res.send("My About page");
+})
+
+app.listen(Port,()=>{
+    console.log(`Server run on ${Port}`);
+})
